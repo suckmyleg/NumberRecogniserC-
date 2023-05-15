d5954a1813a235ab34ae8431557e94c23643b8f6
SuckMyLeg <juanito-146@hotmail.es>
2023-05-15 13:43:16 +0200

    Saving

--- Recogniser/Recogniser/02logic/ImageLoader.cs
+++ Recogniser/Recogniser/02logic/ImageLoader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,29 @@ namespace Recogniser._02logic
 {
     internal class ImageLoader
     {
+        private string imagesPath;
+        private string filename;
+        private bool loadedImage;
+        private int[] imageVector;
+        private int[][] image;
+
+        private const Vector size;
+        private const int totalPixels;
+
+        public ImageLoader(string imagesPath, Vector size) { 
+            this.imagesPath = imagesPath;
+            this.size = size;
+
+            this.totalPixels = this.size[0] * this.size[1];
+        }
+        public void loadImage(string filename) {
+            this.loadImage = false;
+            this.filename = filename;
+            this.imageVector = new int[];
+
+        }
+        public getImage() { }
+        public getImageVector() { }
+
     }
 }
